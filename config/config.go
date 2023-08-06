702e764df926b97df4f608a2594baa519b3ede05
hoyirul <irulgraspama621@gmail.com>
2023-08-06 15:55:27 +0700

    Add Connection with microservices

--- config/config.go
+++ config/config.go
@@ -3,15 +3,17 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func LoadEnv() error {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("./../../.env")
 	if err != nil {
 		return fmt.Errorf("failed to load .env file: %w", err)
 	}
@@ -25,19 +27,19 @@ func InitDB(service string) error {
 		return err
 	}
 
-	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	// 	os.Getenv("DB_USER_PRODUCT"),
-	// 	os.Getenv("DB_PASS_PRODUCT"),
-	// 	os.Getenv("DB_HOST_PRODUCT"),
-	// 	os.Getenv("DB_PORT_PRODUCT"),
-	// 	os.Getenv("DB_NAME_PRODUCT"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv(fmt.Sprintf("DB_USER_%s", service)),
+		os.Getenv(fmt.Sprintf("DB_PASS_%s", service)),
+		os.Getenv(fmt.Sprintf("DB_HOST_%s", service)),
+		os.Getenv(fmt.Sprintf("DB_PORT_%s", service)),
+		os.Getenv(fmt.Sprintf("DB_NAME_%s", service)))
 
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	return fmt.Errorf("failed to connect to database: %w", err)
-	// }
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
 
-	// DB = db
+	DB = db
 	return nil
 }
 
@@ -45,4 +47,6 @@ func ConnectDB(service string) {
 	if err := InitDB(service); err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
+
+	fmt.Printf("Connected to the %s database!", service)
 }
--- services/user/main.go
+++ services/user/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	config.ConnectDB("PRODUCT")
+	config.ConnectDB("USER")
 }
